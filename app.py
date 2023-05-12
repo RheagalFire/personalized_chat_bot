a6550573b2f611f1b7761ea0a14f776f1f97ad5f
RheagalFire <arishalam121@gmail.com>
2023-05-12 15:49:31 +0530

    Streamlit App added

---
+++ app.py
@@ -0,0 +1,128 @@
+import streamlit as st
+from streamlit_chat import message
+from langchain.memory import ConversationBufferMemory
+from langchain import OpenAI, LLMChain, PromptTemplate
+from langchain.chains import ConversationChain
+import openai
+import db
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+# Load .env files
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+st.title('Personalized Bot')
+
+
+# choose the personality
+current_person = st.radio(
+    "ChatBot Beahave like",
+    ('Aarish', 'Prady'),
+    horizontal=True,
+    key="personality"
+)
+
+#make the context using openAI
+embed_model = "text-embedding-ada-002"
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+    
+if 'input' not in st.session_state:
+    st.session_state['input'] = ""
+    
+
+input_query = st.text_input("Enter your query here 👇", st.session_state["input"] , key="input")
+
+
+
+query = st.sidebar.radio(
+	"Select your preference",
+	('Food preferences', 'Game preferences', 'Travel preferences', 'Movie preferences'),
+	key="preference"
+)
+
+if query:
+    st.session_state['last_preference'] = query
+    st.session_state['chat_started'] = True
+    st.session_state['show_chat'] = True
+
+    openai.api_key = OPENAI_API_KEY
+    res = openai.Embedding.create(
+        input=[query],
+        engine=embed_model
+    )
+    xq = res['data'][0]['embedding']
+
+    # get relevant contexts (including the questions)
+    result = db.index.query(xq, top_k=10, include_metadata=True,namespace='text_field_9')
+
+    contexts = [item['metadata']['text'] for item in result['matches']]
+else:
+     st.write('Please set a preference before starting the chat.')
+
+     
+string1 = """
+Using the provided conversation data between two persons, your task is to emulate the speaking style and language of one of the individuals while answering the user's question.
+Draw upon your own knowledge and training to provide relevant and informative responses, but ensure that your answers reflect the mannerisms, tone, and language used by the chosen person. 
+Always respond in the language of the conversation data shared, regardless of the question asked.
+Chosen person is
+"""
+
+string2 = """.
+{chat_history}
+human:{human_input}
+Chatbot:""
+"""
+
+## instructions given to gpt
+primer = string1 + current_person + string2
+
+
+llm = OpenAI(openai_api_key=OPENAI_API_KEY,temperature=0)
+
+prompt = PromptTemplate(
+	input_variables=["chat_history", "human_input"],
+	template=primer
+)
+
+if 'entity_memory' not in st.session_state:
+	st.session_state.entity_memory = ConversationBufferMemory(memory_key="chat_history")
+
+memory = st.session_state.entity_memory
+
+llm_chain = LLMChain(
+	llm=llm, 
+	prompt=prompt, 
+	verbose=True, 
+	memory=memory,
+)
+
+
+if (input_query):
+	augmented_query = "\n\n---\n\n".join(contexts)+"\n\n-----\n\n"+ input_query
+	response = ""
+		
+	response = llm_chain.predict(human_input=augmented_query)
+	
+	## storing session states
+	st.session_state.past.append(input_query)
+	st.session_state.generated.append(response)
+	st.session_state.entity_memory = memory
+
+
+## rendering the messages
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
+message("Hello! I am a personalized bot trained to behave like Arish Alam. Ask me any thing and I will reply in the way he does.") 
+
+
